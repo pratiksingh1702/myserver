41e3a8e336ad979eb98e094597e22ce18fa4fe92
pratiksingh1702 <135985384+pratiksingh1702@users.noreply.github.com>
2025-04-22 19:03:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
